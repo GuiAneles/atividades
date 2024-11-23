29421635463476fc1b45567dc9f515290bb803f1
Seleris <148480902+GuiAneles@users.noreply.github.com>
2024-11-22 23:17:32 -0300

    NumerosParImpar.java

--- Atividades/354/NumerosParImpar.java
+++ Atividades/354/NumerosParImpar.java
@@ -2,16 +2,14 @@ import javax.swing.JOptionPane;
 
 public class NumerosParImpar {
     public static void main(String[] args) {
-        int[] num = new int[15]; // Vetor para armazenar 15 números inteiros
+        int[] num = new int[15];
         String resultado = "Relação Dos Números:\n";
-
-        // Trecho de entrada
+        
         for (int i = 0; i < 15; i++) {
             String s = JOptionPane.showInputDialog("Digite O " + (i + 1) + "º Número:");
             num[i] = Integer.parseInt(s);
         }
 
-        // Trecho de saída
         for (int i = 0; i < 15; i++) {
             resultado += (i + 1) + "- " + num[i];
             if (num[i] % 2 == 0) {
@@ -21,7 +19,6 @@ public class NumerosParImpar {
             }
         }
 
-        // Exibir a listagem completa
         JOptionPane.showMessageDialog(null, resultado);
     }
 }
