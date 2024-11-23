0d08af9478c7b67980a7e20147a6049111d64a21
Seleris <148480902+GuiAneles@users.noreply.github.com>
2024-11-22 23:16:44 -0300

    NomePeloNumero.java

--- Atividades/351/NomePeloNumero.java
+++ Atividades/351/NomePeloNumero.java
@@ -2,15 +2,13 @@ import javax.swing.JOptionPane;
 
 public class NomePeloNumero {
     public static void main(String[] args) {
-        String[] nomes = new String[5]; // Vetor para armazenar os 5 nomes
+        String[] nomes = new String[5];
         int num;
 
-        // Coleta de nomes
         for (int i = 0; i < 5; i++) {
             nomes[i] = JOptionPane.showInputDialog("Digite O Nome Da Pessoa " + (i + 1) + ":");
         }
 
-        // Entrada do número para procurar pelo índice
         do {
             String s = JOptionPane.showInputDialog("Digite O Número Da Pessoa (1 a 5):");
             num = Integer.parseInt(s);
@@ -20,7 +18,6 @@ public class NomePeloNumero {
             }
         } while (num < 1 || num > 5);
 
-        // Exibindo o nome correspondente
         JOptionPane.showMessageDialog(null, "O Nome Correspondente Ao Número " + num + " É: " + nomes[num - 1]);
     }
 }
