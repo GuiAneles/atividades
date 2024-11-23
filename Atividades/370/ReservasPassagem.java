6f7d7210717ea4cafaf7440a360a6900ce5b5558
Seleris <148480902+GuiAneles@users.noreply.github.com>
2024-11-22 23:25:03 -0300

    ReservasPassagem.java

--- Atividades/370/ReservasPassagem.java
+++ Atividades/370/ReservasPassagem.java
@@ -2,19 +2,16 @@ import javax.swing.JOptionPane;
 
 public class ReservasPassagem {
     public static void main(String[] args) {
-        // Leitura do número de vôos e a quantidade de lugares disponíveis
         int numVoo = Integer.parseInt(JOptionPane.showInputDialog("Digite O Número De Bôos Disponíveis:"));
         Voo[] voos = new Voo[numVoo];
 
         for (int i = 0; i < numVoo; i++) {
             int numeroVoo = Integer.parseInt(JOptionPane.showInputDialog("Digite O Número Do Vôo " + (i + 1) + ":"));
             int lugares = Integer.parseInt(JOptionPane.showInputDialog("Digite O Número De Lugares Disponíveis Para O Bôo " + numeroVoo + ":"));
-            voos[i] = new Voo(numeroVoo, lugares); // Cria o objeto Voo e armazena no vetor
+            voos[i] = new Voo(numeroVoo, lugares);
         }
-
-        // Reservas de passagem
+        
         while (true) {
-            // Leitura do número da identidade e do número do vôo desejado
             String identidade = JOptionPane.showInputDialog("Digite O Número Da Identidade Do Cliente ('sair' Para Encerrar):");
 
             if ("sair".equalsIgnoreCase(identidade)) {
@@ -23,13 +20,11 @@ public class ReservasPassagem {
 
             int numeroVooDesejado = Integer.parseInt(JOptionPane.showInputDialog("Digite O Número Do Vôo Desejado:"));
 
-            // Verificação se o vôo existe e se há lugares disponíveis
             boolean reservaRealizada = false;
 
             for (int i = 0; i < numVoo; i++) {
                 if (voos[i].numeroVoo == numeroVooDesejado) {
                     if (voos[i].lugaresDisponiveis > 0) {
-                        // Realiza a reserva e atualiza o número de lugares disponíveis
                         voos[i].lugaresDisponiveis--;
                         JOptionPane.showMessageDialog(null, "Reserva Realizada Com Sucesso! \n Identidade: " + identidade + "\n Vôo: " + numeroVooDesejado);
                         reservaRealizada = true;
