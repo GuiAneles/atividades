71c11ae0683d8634ce9a3833ab5581fad7c32753
Seleris <148480902+GuiAneles@users.noreply.github.com>
2025-02-02 18:08:57 -0300

    Update Expressao.java

--- Atividades 8/VerificExpressões/Expressao.java
+++ Atividades 8/VerificExpressões/Expressao.java
@@ -3,10 +3,8 @@ import javax.swing.JOptionPane;
 public class Expressao {
 
     public static void main(String[] args) {
-        // Solicita a expressão ao usuário através de uma caixa de diálogo
         String expressao = JOptionPane.showInputDialog("Digite Uma Expressão Aritmética Para Verificar A Parametrização:");
 
-        // Verifica se a expressão foi fornecida
         if (expressao == null || expressao.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Erro: Nenhuma Expressão Fornecida.");
             return;
