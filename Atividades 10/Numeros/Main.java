db7152fc6d4c6cd5ef89d29df01d42d24de00405
Seleris <148480902+GuiAneles@users.noreply.github.com>
2025-02-12 21:14:33 -0300

    Add files via upload

---
+++ Atividades 10/Numeros/Main.java
@@ -0,0 +1,39 @@
+//n usei JOptionPane pq é biblioteca nativa
+public class Main {
+    public static void main(String[] args) {
+        int[] vetor = new int[1000];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = gerarNumeroAleatorio(-9999, 9999);
+        }
+
+        System.out.println("Números Gerados:");
+        for (int num : vetor) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        ListaDE lista = new ListaDE();
+        for (int num : vetor) {
+            lista.inserirOrdenado(num);
+        }
+
+        System.out.println("Lista Em Ordem Crescente:");
+        lista.imprimirCrescente();
+
+        System.out.println("Lista Em Ordem Decrescente:");
+        lista.imprimirDecrescente();
+
+        lista.removerPrimos();
+
+        System.out.println("Lista Após Remover Números Primos:");
+        lista.imprimirCrescente();
+    }
+
+
+    private static int gerarNumeroAleatorio(int min, int max) {
+        long tempo = System.nanoTime();
+        int numero = (int) ((tempo % (max - min + 1))) + min;
+        return numero;
+    }
+}
