b1de9e2c1a49774c753041852264a530c3622ee6
Seleris <148480902+GuiAneles@users.noreply.github.com>
2025-02-12 21:16:19 -0300

    Add files via upload

---
+++ Atividades 10/Numeros/Main.java
@@ -0,0 +1,42 @@
+//n usei JOptionPane pq é biblioteca nativa
+public class Main {
+    public static void main(String[] args) {
+        int[] vetor = new int[1000];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = gerarNumeroAleatorio(-9999, 9999);
+        }
+
+        /*na atividade o senhor fala pra: "imprimir a lista em ordem crescente e depois em ordem decrescente."
+        mas no exemplo imprime numero por numero, como são mil numeros vou só imprimir a lista mesmo*/
+        
+        System.out.println("Numeros Gerados:");
+        for (int num : vetor) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        ListaDE lista = new ListaDE();
+        for (int num : vetor) {
+            lista.inserirOrdenado(num);
+        }
+
+        System.out.println("Lista Em Ordem Crescente:");
+        lista.imprimirCrescente();
+
+        System.out.println("Lista Em Ordem Decrescente:");
+        lista.imprimirDecrescente();
+
+        lista.removerPrimos();
+
+        System.out.println("Lista Apos Remover Numeros Primos:");
+        lista.imprimirCrescente();
+    }
+
+
+    private static int gerarNumeroAleatorio(int min, int max) {
+        long tempo = System.nanoTime();
+        int numero = (int) ((tempo % (max - min + 1))) + min;
+        return numero;
+    }
+}
